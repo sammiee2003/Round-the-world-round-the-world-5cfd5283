aea2af44088b6a44da1b91464e7b21392063f80f
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-25 15:17:22 +0100

    lvl 4 opdr 5

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,29 +1,28 @@
 <?php
-$bedrag = readline(" ");
-$geldEnheden = array(50,20,10,5,2,1);
-$centen = array(50, 20, 10, 5, 2, 1);
-$geld = intval($bedrag);
-$cent = $bedrag - $geld;
+$input = ($argv[1]);
+$geldeenheden = array(50, 20, 10, 5, 2, 1);
+$centen = array(50, 20 ,10 ,5);
+$geld = intval($input);
+$cent = $input - $geld;
 $cent = intval(round($cent * 100));
 $cent = round($cent / 5) * 5;
-
-euro($geldEnheden, $geld);
-function euro($geldEnheden, $geld){
-    foreach ($geldEnheden as $value) {
+euro($geldeenheden, $geld);
+function euro($geldeenheden, $geld) {
+    foreach ($geldeenheden as $value) {
         $restgeld = floor($geld / $value);
-        if ($restgeld >= 1){
+        if ($restgeld >= 1) {
             $geld = $geld - ($value * $restgeld);
-            echo $restgeld . "x" . $value . "euro" . PHP_EOL;
+            echo $restgeld . " x " . $value . " euro" . PHP_EOL;
         }
     }
 }
 centen($centen, $cent);
-function centen($centen, $cent){
+function centen($centen, $cent) {
     foreach ($centen as $value) {
         $restcent = floor($cent / $value);
-        if ($restcent >= 1){
+        if ($restcent >= 1) {
             $cent = $cent - ($value * $restcent);
-            echo $restcent . " x" . $value . "cent" . PHP_EOL;
+            echo $restcent . " x " . $value . " cent" . PHP_EOL;
         }
     }
 }
